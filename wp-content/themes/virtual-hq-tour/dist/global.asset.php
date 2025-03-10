71b88a83684197ff0c1db7526974fdcd4d763cf8
KJ Roelke <kroelke@choctawnation.com>
2025-03-10 09:59:04 -0500

    delete the dist folder and add actions

--- wp-content/themes/virtual-hq-tour/dist/global.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '1563053248b909f20b01');
--- wp-content/themes/virtual-hq-tour/dist/modules/cnoMasonry.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '3e592c5e8865da731f91');
